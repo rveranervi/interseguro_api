e0d663f8957bd081b5a3b994a352f5d18af652b7
rveranervi <rveranervi@gmail.com>
2021-03-10 15:28:40 -0500

    API COMPLETA
    Api de consumo para examen de Interseguro.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# interapi
- 
+# API LARAVEL
+
+- Desarrollado por Ricardo Vera Nervi
+- Funcionamiento: Esta API genera un arreglo y da opción a girarlo en sentido horario o antihorario.
+
+## Endpoints
+
+- /obtener_arreglo -> n_dimensiones(INT): Retorna el arreglo de elementos segun la cantidad de dimensiones solicitada.
+- /reordenar_horario -> arreglo(INT): Retorna el arreglo ordenado en sentido HORARIO.
+- /reordenar_antihorario -> arreglo(INT): Retorna el arreglo ordenado en sentido ANTIHORARIO.
+
+## Evidencias
+
+Las evidencias en screenshots las puede ver en la carpeta evidencias. En esta carpeta habrá imagenes del consumo del api por POSTMAN.
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function (Request $request) {
+    return view('welcome');
+});
+
+Route::post('/obtener_arreglo', function (Request $request) {
+    $arreglo = array();
+    for ($i=0; $i < $request->n_dimensiones; $i++) { 
+    	$fila = array();
+    	for ($z=0; $z < $request->n_dimensiones; $z++) { 
+	    	$fila[$z] = rand(0,25);
+	    }
+	    $arreglo[$i] = $fila;
+    }
+    return serialize($arreglo);
+});
+
+Route::post('/reordenar_horario', function (Request $request) {
+    $arreglo = unserialize($request->arreglo);
+    $height = count($arreglo);
+    $width = count($arreglo[0]);
+    $arreglo90 = array();
+
+    for ($i = 0; $i < $width; $i++) {
+        for ($j = 0; $j < $height; $j++) {
+            $arreglo90[$height - $i - 1][$j] = $arreglo[$height - $j - 1][$i];
+        }
+    }
+    return serialize($arreglo90);
+});
+
+Route::post('/reordenar_antihorario', function (Request $request) {
+    $arreglo = unserialize($request->arreglo);
+    $height = count($arreglo);
+    $width = count($arreglo[0]);
+    $arreglo90 = array();
+
+    for ($i = $width-1; $i >= 0; $i--) {
+        for ($j = $height-1; $j >= 0; $j--) {
+            $arreglo90[$height - $i - 1][$j] = $arreglo[$height - $j - 1][$i];
+        }
+    }
+    return serialize($arreglo90);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
